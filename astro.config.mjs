37b13ccb0a7429ee448f67fbca6b9c3d71ed5506
marh08192003 <riverita470@gmail.com>
2026-06-18 17:24:27 -0500

    chore: setup github actions deployment pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // URL absoluta de tu sitio en producción
+  site: "https://marh08192003.github.io",
+  // Ruta base correspondiente al nombre de tu repositorio
+  base: "/Portfolio",
+
   vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
 
