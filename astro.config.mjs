070c89444a1431f223245775cbc9b28643908056
marh08192003 <riverita470@gmail.com>
2026-06-18 21:54:39 -0500

    fix: integrate vite-tsconfig-paths to resolve build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
-  // URL absoluta de tu sitio en producción
   site: "https://marh08192003.github.io",
-  // Ruta base correspondiente al nombre de tu repositorio
   base: "/Portfolio",
 
   vite: {
-    // @ts-ignore
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), tsconfigPaths()],
   },
 
   markdown: {},
